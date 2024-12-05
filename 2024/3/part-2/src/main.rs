eac45a979aaba87e458c100edc5c10d3e947bb51
Devin Droddy <devin.droddy@gmail.com>
2024-12-05 14:19:55 -0500

    2024 day 3 part 2 solution

--- 2024/3/part-2/src/main.rs
+++ 2024/3/part-2/src/main.rs
@@ -1,7 +1,9 @@
 use regex::Regex;
+use std::collections::VecDeque;
+use std::ops::Range;
 
 fn main() {
-    let input = include_str!("../../example_input.txt");
+    let input = include_str!("../../input.txt");
     let mul_regex = Regex::new(r"mul\(([0-9]*),([0-9]*)\)").unwrap();
     let do_regex = Regex::new(r"do\(\)").unwrap();
     let dont_regex = Regex::new(r"don't\(\)").unwrap();
@@ -9,17 +11,28 @@ fn main() {
         .captures_iter(input)
         .map(|c| (c[1].parse::<u32>().unwrap(), c[2].parse::<u32>().unwrap()));
     let muls = mul_regex.find_iter(input);
-    let mut dos = do_regex.find_iter(input).collect::<Vec<_>>();
+    let mut dos = do_regex.find_iter(input).collect::<VecDeque<_>>();
     let donts = dont_regex.find_iter(input).collect::<Vec<_>>();
     let mut disabled_ranges = Vec::new();
 
-    if dos[0].start() < donts[0].start() {
-        dos.pop();
-    }
-
     for dont_match in donts {
-        if let Some(do_match) = dos.pop() {
-            disabled_ranges.push(dont_match.start()..do_match.start());
+        if disabled_ranges
+            .iter()
+            .any(|r: &Range<usize>| r.contains(&dont_match.start()))
+        {
+            continue;
+        }
+
+        if dos.is_empty() {
+            disabled_ranges.push(dont_match.start()..input.len());
+        }
+        while let Some(do_match) = dos.pop_front() {
+            if do_match.start() < dont_match.start() {
+                continue;
+            } else {
+                disabled_ranges.push(dont_match.start()..do_match.start());
+                break;
+            }
         }
     }
 
